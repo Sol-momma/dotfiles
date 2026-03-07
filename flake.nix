e9caeeb61abc46ece0894f9e9f323431e5c4af82
So Momma <sol0608sol@gmail.com>
2026-03-07 15:37:49 +0900

    init nix flake environment

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "Personal dotfiles and CLI development shell";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+
+  outputs =
+    { self, ... }@inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs, system }:
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              self.formatter.${system}
+              zoxide
+              eza
+              lazygit
+              fzf
+              ripgrep
+              bat
+              fd
+            ];
+
+            env = { };
+
+            shellHook = ''
+              echo "Nix CLI shell activated"
+            '';
+          };
+        }
+      );
+
+      formatter = forEachSupportedSystem ({ pkgs, ... }: pkgs.nixfmt);
+    };
+}
