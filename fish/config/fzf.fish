1c2c9d5b9ca08ee275bab149e87f480df7ee39c8
So Momma <sol0608sol@gmail.com>
2026-03-08 02:50:06 +0900

    Enhance FZF configuration in Fish shell for improved file searching and completion

--- fish/config/fzf.fish
+++ fish/config/fzf.fish
@@ -1,3 +1,6 @@
 set -gx FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --no-messages'
 set -gx FZF_DEFAULT_OPTS '--extended --cycle --select-1 --height 40% --reverse --border'
+set -gx FZF_FIND_FILE_OPTS '--preview "bat --color=always --style=header,grid --line-range :100 {}"'
 set -gx FZF_CTRL_R_OPTS "--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
+set -gx FZF_LEGACY_KEYBINDINGS 0
+set -gx FZF_COMPLETION_TRIGGER '**'
